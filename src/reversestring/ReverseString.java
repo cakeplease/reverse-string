a9595bbf0dd5dfe1658c7bc9cc99e974ca91b64b
Kasia <kasia.szlejter@gmail.com>
2019-10-14 21:56:01 +0200

    Uploading reverseString class

---
+++ src/reversestring/ReverseString.java
@@ -0,0 +1,31 @@
+package reversestring;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kasia
+ * @version 1.0
+ */
+public class ReverseString {
+    
+    public static String reverseString(String stringToReverse) {
+	String reversedString = new StringBuilder(stringToReverse).reverse().toString();
+	return reversedString;
+    }
+            
+    public static void main(String[] args) {
+	boolean running = true;
+	while(running) {
+	    System.out.println("Enter string to reverse or write quite to exit");
+	    Scanner scanner = new Scanner(System.in);
+	    String userString = scanner.nextLine();
+	    
+	    
+	    if (userString.equals("quit")) {
+		running = false;
+	    } else {
+		System.out.println(reverseString(userString));
+	    }
+	}
+    }
+}
